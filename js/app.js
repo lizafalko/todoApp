7bae26fc66f77fe386b2f89fb9bb6ef98a8e90a8
lizafalko <liza199797@mail.ru>
2018-05-23 11:14:22 +0300

    added start js app

---
+++ js/app.js
@@ -0,0 +1,40 @@
+function createTodoItem(title) {
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.className = 'checkbox';
+
+  const label = document.createElement('label');
+  label.innerText = title;
+  label.className = 'title';
+
+  const editInput = document.createElement('input');
+  editInput.type = 'text';
+  editInput.className = 'textfield';
+
+  const editButton = document.createElement('button');
+  editButton.innerText = 'Изменить';
+  editButton.className = 'edit';
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = 'Удалить';
+  deleteButton.className = 'delete';
+
+  const listItem = createElement('li');
+  listItem.className = 'todo-item';
+}
+
+function addTodoItem(event) {
+  event.preventDefault();
+
+  if (addInput.value === '')
+    return alert('Необходимо ввести название задачи.');
+
+    const listItem = createTodoItem(addInput.value);
+}
+
+const todoform = document.getElementById('todo-form');
+const addInput = document.getElementById('add-input');
+const todoList = document.getElementById('todo-list');
+const todoItems = document.querySelectorAll('.todo-item');
+
+todoForm.addEventListener('submit', addTodoItem);
